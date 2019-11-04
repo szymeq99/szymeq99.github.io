2730fa7a9812cd332a3e8a5e5c14e335af0e02a6
Szymon Krok <szymon-89@tlen.pl>
2019-11-04 10:10:51 +0100

    zadanie sass

---
+++ zadanie-sass-przed/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp'); 
+const sass = require('gulp-sass'); 
+
+
+gulp.task('sass',() => {   
+    return gulp.src('app/scss/**/*.scss')   
+    .pipe(sass())            
+    .pipe(gulp.dest('app/css'))      
+})  
+
+gulp.task('watch', () => {
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));  
+})
