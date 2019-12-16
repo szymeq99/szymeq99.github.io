17b2b304ded5bdc827095691cb52e16c5b6332d7
Szymon Krok <szymon-89@tlen.pl>
2019-12-16 21:50:01 +0100

    Add normalize css, basic styles and javascript variables

---
+++ Scroller-feature/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+    
+    document.addEventListener('mousewheel', (e) => {
+        const 
+    })
+})
