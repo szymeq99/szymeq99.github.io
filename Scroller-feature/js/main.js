35d782310ac04a37ae52500a5507cc05ae6410e1
Szymon Krok <szymon-89@tlen.pl>
2019-12-16 22:06:30 +0100

    Add scroll feature

--- Scroller-feature/js/main.js
+++ Scroller-feature/js/main.js
@@ -2,8 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const rootElement = document.querySelector('#root');
     const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
+    let isThrottled = false;
     
     document.addEventListener('mousewheel', (e) => {
-        const 
+        if(isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(() => {
+            isThrottled = false;
+        },1000);
+        
+        const direction = e.wheelDelta < 0 ? 1 : -1;
+
+        if(direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1;
+            if(isLastSection) return; 
+        } else if (direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if(firstSection) return;
+        } 
+
+        currentSectionIndex += direction;
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
     })
 })
