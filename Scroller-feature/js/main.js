cec5ea76bc4ad80dcde55c881392b675df90dbbe
Szymon Krok <szymon-89@tlen.pl>
2019-12-18 13:39:54 +0100

    Add support for keyboard

--- Scroller-feature/js/main.js
+++ Scroller-feature/js/main.js
@@ -2,5 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const scroller = new Scroller('#root');
     
-    document.addEventListener('mousewheel', scroller.listenScroll)
+    document.addEventListener('mousewheel', scroller.listenScroll);
+    document.addEventListener('keydown',e => {
+        switch (e.keyCode) {
+            case 40:
+                return scroller.scroll(1);
+                break;
+        
+             case 38:
+                 return scroller.scroll(-1);
+             break;
+
+
+            default:
+                return;
+        }
+    }
+    )
 })
